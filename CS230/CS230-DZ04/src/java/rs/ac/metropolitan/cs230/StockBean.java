ccc6adfdf3768c25cf350c4a806d62a023201eac
Aleksa <aleksa.cekic8@gmail.com>
2021-04-15 11:30:57 +0200

    3. and 4. homework for cs230

---
+++ CS230/CS230-DZ04/src/java/rs/ac/metropolitan/cs230/StockBean.java
@@ -0,0 +1,17 @@
+package rs.ac.metropolitan.cs230;
+
+public class StockBean {
+
+    private String simbol;
+
+    public StockBean() {
+    }
+
+    public String getSimbol() {
+        return simbol;
+    }
+
+    public void setSimbol(String simbol) {
+        this.simbol = simbol;
+    }
+}
