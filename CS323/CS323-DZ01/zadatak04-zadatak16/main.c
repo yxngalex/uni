cf86ed537a7f1c1bb37543e0e02f1a714d8f2f65
Aleksa Cekic <aleksa.cekic8@gmail.com>
2022-02-28 21:29:04 +0100

    CS323-DZ01

--- CS323/CS323-DZ01/zadatak04-zadatak16/main.c
+++ CS323/CS323-DZ01/zadatak04-zadatak16/main.c
@@ -2,6 +2,18 @@
 #include <stdlib.h>
 #include <time.h>
 
+/*
+    Zadatak 4. Napisati program koji simulira bacanje kocke, tj. daje slucajan broj iz intevala[1, 6].
+    Koristiti funkciju rand() iz standardne biblioteke stdlib.h
+  */
+
+/*
+    Zadatak 16. Napisati C program koji racuna sledeci zadatak:
+        9.5 X 4.5 - 2.5 X 3
+       ----------------------
+            45.5 - 3.5
+  */
+
 int main()
 {
     int r;
