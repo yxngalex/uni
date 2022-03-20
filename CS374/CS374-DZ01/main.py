727503e5d946df3f9537a1415d6fd3c39f3741ff
Aleksa Cekic <aleksa.cekic8@gmail.com>
2022-03-20 21:55:51 +0100

    CS374-DZ01

--- CS374/CS374-DZ01/main.py
+++ CS374/CS374-DZ01/main.py
@@ -120,7 +120,7 @@ class Cube:
 def h(p1, p2):
     x1, y1 = p1
     x2, y2 = p2
-    return math.sqrt((abs((x1 - x2) ** 2) + abs((y1 - y2) ** 2)))
+    return math.sqrt(((abs((x1 - x2)) ** 2) + (abs((y1 - y2)) ** 2)))
 
 
 def reconstructPath(camefrom, end, draw):
