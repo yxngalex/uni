cf8263ea7973df197078b89c39517a6d502e1b0e
Aleksa Cekic <aleksa.cekic8@gmail.com>
2022-05-29 01:40:47 +0200

    CS323-DZ12

--- CS323/CS323-DZ12/Zaposleni.cpp
+++ CS323/CS323-DZ12/Zaposleni.cpp
@@ -6,56 +6,55 @@ Zaposleni::Zaposleni() {
 }
 
 Zaposleni::~Zaposleni() {
-	cout << "Zaposleni je obrisan" << endl;
+    cout << "Zaposleni je obrisan" << endl;
 }
 
 void Zaposleni::setIme(string ime) {
-	this->ime = ime;
+    this->ime = ime;
 }
 
 void Zaposleni::setPrezime(string prezime) {
-	this->prezime = prezime;
+    this->prezime = prezime;
 }
 
 void Zaposleni::setAdresa(string adresa) {
-	this->adresa = adresa;
+    this->adresa = adresa;
 }
 
 void Zaposleni::setTelefon(string telefon) {
-	this->telefon = telefon;
+    this->telefon = telefon;
 }
 
 void Zaposleni::setGodiste(string godiste) {
-	this->godiste = godiste;
+    this->godiste = godiste;
 }
 
 void Zaposleni::setDatumRodj(string datumRodj) {
-	this->datumRodj = datumRodj;
+    this->datumRodj = datumRodj;
 }
 
 string Zaposleni::getIme() const {
-	return ime;
+    return ime;
 }
 string Zaposleni::getPrezime() const {
-	return prezime;
+    return prezime;
 }
 string Zaposleni::getAdresa() const {
-	return adresa;
+    return adresa;
 }
 
 string Zaposleni::getTelefon() const {
-	return telefon;
+    return telefon;
 }
 
 string Zaposleni::getGodiste() const {
-	return godiste;
+    return godiste;
 }
 
 string Zaposleni::getDatumRodj() const {
-	return datumRodj;
+    return datumRodj;
 }
 
 void Zaposleni::info() {
-	cout << "Ime: " << this->ime << ", Prezime: " << this->prezime << " , Godiste: " << this->godiste << " , Adresa: " << this->adresa << " , Datum Rodjenja: " << this->datumRodj <<  endl;
+    cout << "Ime: " << this->ime << ", Prezime: " << this->prezime << " , Godiste: " << this->godiste << " , Adresa: " << this->adresa << " , Datum Rodjenja: " << this->datumRodj <<  endl;
 }
-
---
+++ CS323/CS323-DZ12/Zaposleni.h
@@ -0,0 +1,35 @@
+#pragma once
+#include<iostream>
+using namespace std;
+
+class Zaposleni
+{
+    // 14. zadatak
+protected:
+    string ime;
+    string prezime;
+    string adresa;
+    string telefon;
+    string godiste;
+    string datumRodj;
+
+public:
+    Zaposleni();
+    ~Zaposleni();
+
+    void setIme(string ime);
+    void setPrezime(string prezime);
+    void setAdresa(string adresa);
+    void setTelefon(string telefon);
+    void setGodiste(string godiste);
+    void setDatumRodj(string datumRodj);
+
+    string getIme() const;
+    string getPrezime() const;
+    string getAdresa() const;
+    string getTelefon() const;
+    string getGodiste() const;
+    string getDatumRodj() const;
+
+    void info();
+};
