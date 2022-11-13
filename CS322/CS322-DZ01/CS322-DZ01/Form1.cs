9cef3bd4d595915c69223add50657c8ba049d4b9
Aleksa Cekić <aleksa.cekic8@gmail.com>
2022-11-13 22:10:35 +0100

    CS322-DZ01

---
+++ CS322/CS322-DZ01/CS322-DZ01/Form1.cs
@@ -0,0 +1,20 @@
+namespace CS322_DZ01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
