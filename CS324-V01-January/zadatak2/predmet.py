77a4c88b15687c0bcb431ea37011d07fa89260de
Aleksa Cekić <aleksa.cekic8@gmail.com>
2023-02-18 16:31:22 +0100

    removed CS324-V01

--- CS324-V01-January/zadatak2/predmet.py
+++
@@ -1,6 +0,0 @@
-class Predmet:
-    def __init__(self, sifra, naziv, ocena, datum_polaganja):
-        self.sifra = sifra
-        self.naziv = naziv
-        self.ocena = ocena
-        self.datum_polaganja = datum_polaganja
